f454988a39f3dc9ea6a74cf057cf8eb5dd2c34cd
jonamsalem <133527937+jonamsalem@users.noreply.github.com>
2023-07-19 16:30:49 -0400

    Jon (#2)
    * fixed doubles
    
    * CRUD for products

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const app = express()
 const { dbConnect } = require("./db")
 let userController = require("./controllers/auth")
+let productController = require("./controllers/products")
 let sessionValidation = require("./middlewares/sessionValidation")
 const PORT = process.env.PORT || 4000
 const HOST = process.env.HOST || "127.0.0.1"
@@ -12,9 +13,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use("/admin", userController)
+app.use("/products", productController)
 
 app.listen(PORT, HOST, () => {
     dbConnect()
     console.log(`[server] listening on ${HOST}: ${PORT}`
-    )
+    )  
 })
--- controllers/products.js
+++ controllers/products.js
@@ -19,10 +19,14 @@ const upload = multer({
     }
 })
 
-application.post("/api/products", upload.single("image"), async (req, res) =>{ //upload.single("image") is middlware that processes an incoming file - this is part of multer
+router.post("/create", upload.single("file"), async (req, res) =>{ //upload.single("image") is middlware that processes an incoming file - this is part of multer
     try{
-        const { altText, productName, price, description, quantity, tag } = req.body
-        const imageUrl = req.file.buffer //file.buffer is a property of multer middleware. Processed file's buffer is accessible Access binary data
+        let file = req.file
+        const imageUrl = file.buffer //file.buffer is a property of multer middleware. Processed file's buffer is accessible Access binary data
+        const { altText, productName, price, description, quantity, tag} = req.body
+        console.log(altText, productName, price, quantity, tag)
+        if ( !altText ||!productName ||!price ||!quantity ||!tag) throw new Error ("All fields are required")
+
 
         const s3Params = {
             Bucket: process.env.POTTERY_BUCKET_NAME,
@@ -31,6 +35,8 @@ application.post("/api/products", upload.single("image"), async (req, res) =>{ /
             ACL: "public-read",//bucket is private, but will allow users to see image on client side. ACL = Access Control List (controls aces to objects store in S3 bucket)
             ContentType: req.file.mimtype,
         };
+
+        const data = await s3.upload(s3Params).promise();
 
         const newProduct = new Product ({
             imageUrl: data.Location,
@@ -51,6 +57,106 @@ application.post("/api/products", upload.single("image"), async (req, res) =>{ /
         })
 
     }catch(err){
+        res.status(500).json({
+            message: err.message}),
         console.log(err)
     }
 })
+
+
+router. get("/all", async (req, res) =>{
+    try{
+    let allProducts = await Product.find()
+    if (allProducts.length == 0) throw new Error("No products found")
+    res.status(200).json(allProducts)}
+
+    catch(err){
+        res.status(500).json({
+            message: err.message}),
+        console.log(err)
+    }
+
+})
+
+router. get("/:id", async (req, res) =>{
+    try{
+    let {id} = req.params
+    let oneProduct = await Product.findOne({_id: id})
+    if (!oneProduct) throw new Error("No products found")
+    res.status(200).json(oneProduct)}
+
+    catch(err){
+        res.status(500).json({
+            message: err.message}),
+        console.log(err)
+    }
+
+})
+
+router.delete("/delete/:id", async (req, res) =>{
+    try{
+    let {id} = req.params
+    let oneProduct = await Product.deleteOne({_id: id})
+    console.log(oneProduct)
+    if (oneProduct.deletedCount == 0) throw Error("No products found")
+    res.status(200).json({
+        message: "Product deleted"})}
+    
+    catch(err){
+        res.status(500).json({
+            message: err.message}),
+        console.log(err)
+    }
+
+})
+
+router.put("/update/:id",  upload.none(), async (req, res) =>{
+    try{
+        let {id} = req.params
+        let message = req.body
+        console.log(message)
+        let oneProduct = await Product.updateOne({_id: id},{$set: message})
+        console.log(oneProduct)
+        if (oneProduct.matchedCount == 0) throw Error("No products found")
+        res.status(200).json({
+            message: "Product updated"})}
+    
+    catch(err){
+        res.status(500).json({
+            message: err.message}),
+        console.log(err)
+    }
+})
+
+
+router.put("/updateImg/:_id", upload.single("file"), async (req, res) =>{
+    try{
+        let {_id} = req.params
+        let file = req.file
+        const image = file.buffer
+        const s3Params = {
+            Bucket: process.env.POTTERY_BUCKET_NAME,
+            Key: `${Date.now()}-${req.file.originalname}`,//req.file is a propery of multer middleware and "originalname" is one of its properties
+            Body: image,
+            ACL: "public-read",//bucket is private, but will allow users to see image on client side. ACL = Access Control List (controls aces to objects store in S3 bucket)
+            ContentType: req.file.mimtype,
+        };
+        const data = await s3.upload(s3Params).promise();
+
+        let imageUrl = data.Location
+        console.log(imageUrl)
+
+        let oneProduct = await Product.updateOne({_id: _id},{imageUrl: imageUrl})
+        console.log(oneProduct)
+        if (oneProduct.matchedCount == 0) throw Error("No products found")
+        res.status(200).json({
+            message: "Product updated"})}
+    
+    catch(err){
+        res.status(500).json({
+            message: err.message}),
+        console.log(err)
+    }
+})
+
+module.exports = router
